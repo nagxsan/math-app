533c713a369409172e43bfcf09e10e614ad819f1
nagxsan <sanchetng@gmail.com>
2025-03-02 14:22:14 +0530

    get quizzes api

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,8 +22,9 @@ import {
 
 import {Button} from "@/components/ui/button";
 import axios from "axios";
-import {useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator"
 
 export default function Dashboard() {
   const [studentsList, setStudentsList] = useState<{ email: string }[]>([]);
@@ -36,6 +37,8 @@ export default function Dashboard() {
 
   const [quiz, setQuiz] = useState<any[]>([]);
 
+  const [quizzes, setQuizzes] = useState<any[]>([]);
+
   const token = localStorage.getItem('mathtor-token');
   if (!token) {
     return null;
@@ -44,6 +47,19 @@ export default function Dashboard() {
   if (!data || typeof data === "string") {
     return null;
   }
+
+  useEffect(() => {
+    async function fetchQuizzes() {
+      const quizzes = await axios.get('/api/quiz', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('mathtor-token'),
+        }
+      })
+  
+      setQuizzes(quizzes.data.quizzes);
+    }
+    fetchQuizzes();
+  }, [])
   
   async function getStudentsList() {
     const response = await axios.get('/api/users/get-students', {
@@ -99,6 +115,15 @@ export default function Dashboard() {
           'Authorization': 'Bearer ' + localStorage.getItem('mathtor-token'),
         }
       });
+
+      const quizzes = await axios.get('/api/quiz', {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('mathtor-token'),
+        }
+      })
+
+      setQuizzes(quizzes.data.quizzes);
+
     } catch (e: any) {
       console.log(e?.response?.data || e?.message);
     } finally {
@@ -106,87 +131,132 @@ export default function Dashboard() {
     }
   }
   return (
-    <div className="p-20 flex flex-wrap gap-10">
-      {data?.role === "admin" && (
-        <Dialog>
-          <DialogTrigger className="px-4 py-3 bg-black text-white rounded-lg" onClick={getStudentsList}>
-            View students
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Students</DialogTitle>
-              <DialogDescription>
-                <div className="flex flex-col gap-y-5">
-                  <div>
-                    Students using the platform with scores for quizzes
-                  </div>
-                  {studentsList && studentsList.map((st, idx) => (
-                    <div key={idx}>
-                      {st?.email}
+    <div className="flex flex-col gap-10">
+      <div className="p-20 flex flex-wrap gap-10">
+        {data?.role === "admin" && (
+          <Dialog>
+            <DialogTrigger className="px-4 py-3 bg-black text-white rounded-lg" onClick={getStudentsList}>
+              View students
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Students</DialogTitle>
+                <DialogDescription>
+                  <div className="flex flex-col gap-y-5">
+                    <div>
+                      Students using the platform with scores for quizzes
                     </div>
+                    {studentsList && studentsList.map((st, idx) => (
+                      <div key={idx}>
+                        {st?.email}
+                      </div>
+                    ))}
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        )}
+        {data?.role === "admin" && (
+          <Dialog>
+            <DialogTrigger className="px-4 py-3 bg-black text-white rounded-lg">
+              Add quiz
+            </DialogTrigger>
+            <DialogContent className="max-h-[650px] overflow-y-scroll">
+              <DialogHeader>
+                <DialogTitle>Quiz</DialogTitle>
+                <DialogDescription>
+                  {quiz && quiz.map((question, idx) => (
+                    <Card>
+                      <CardHeader>
+                        <CardTitle>
+                          {`Question ${idx + 1}`}
+                        </CardTitle>
+                        <CardDescription>
+                          {question.question}
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <div className="flex flex-col gap-y-5">
+                          <div>Options:</div>
+                          {question.options.map((option: any, idx: any) => (
+                            <div key={idx}>
+                              {option.number}: {option.value}
+                            </div>
+                          ))}
+                          <div>Correct option: {question.correctOption}</div>
+                        </div>
+                      </CardContent>
+                    </Card>
                   ))}
-                </div>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
-      {data?.role === "admin" && (
-        <Dialog>
-          <DialogTrigger className="px-4 py-3 bg-black text-white rounded-lg">
-            Add quiz
-          </DialogTrigger>
-          <DialogContent className="max-h-[650px] overflow-y-scroll">
-            <DialogHeader>
-              <DialogTitle>Quiz</DialogTitle>
-              <DialogDescription>
-                {quiz && quiz.map((question, idx) => (
                   <Card>
                     <CardHeader>
-                      <CardTitle>
-                        {`Question ${idx + 1}`}
-                      </CardTitle>
+                      <CardTitle>Question 1</CardTitle>
                       <CardDescription>
-                        {question.question}
+                        <Input value={question} onChange={(e) => setQuestion(e.target.value)} />
                       </CardDescription>
                     </CardHeader>
                     <CardContent>
-                      <div className="flex flex-col gap-y-5">
-                        <div>Options:</div>
-                        {question.options.map((option: any, idx: any) => (
-                          <div key={idx}>
-                            {option.number}: {option.value}
-                          </div>
-                        ))}
-                        <div>Correct option: {question.correctOption}</div>
-                      </div>
+                      <Input placeholder="Enter option 1" value={option1} onChange={(e) => setOption1(e.target.value)} required />
+                      <Input placeholder="Enter option 2" value={option2} onChange={(e) => setOption2(e.target.value)} required />
+                      <Input placeholder="Enter option 3" value={option3} onChange={(e) => setOption3(e.target.value)} required />
+                      <Input placeholder="Enter option 4" value={option4} onChange={(e) => setOption4(e.target.value)} required />
+                      <Input placeholder="Enter correct option number (1,2,3,4)" value={correctOption} onChange={(e) => setCorrectOption(e.target.value)} required />
                     </CardContent>
+                    <CardFooter>
+                      <Button variant="secondary" onClick={addQuizQuestion}>Add question</Button>
+                      <Button variant="default" onClick={addQuiz}>Add Quiz</Button>
+                    </CardFooter>
                   </Card>
-                ))}
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Question 1</CardTitle>
-                    <CardDescription>
-                      <Input value={question} onChange={(e) => setQuestion(e.target.value)} />
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Input placeholder="Enter option 1" value={option1} onChange={(e) => setOption1(e.target.value)} required />
-                    <Input placeholder="Enter option 2" value={option2} onChange={(e) => setOption2(e.target.value)} required />
-                    <Input placeholder="Enter option 3" value={option3} onChange={(e) => setOption3(e.target.value)} required />
-                    <Input placeholder="Enter option 4" value={option4} onChange={(e) => setOption4(e.target.value)} required />
-                    <Input placeholder="Enter correct option number (1,2,3,4)" value={correctOption} onChange={(e) => setCorrectOption(e.target.value)} required />
-                  </CardContent>
-                  <CardFooter>
-                    <Button variant="secondary" onClick={addQuizQuestion}>Add question</Button>
-                    <Button variant="default" onClick={addQuiz}>Add Quiz</Button>
-                  </CardFooter>
-                </Card>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        )}
+      </div>
+      <Separator />
+      <div className="flex flex-col gap-10 p-10">
+        <div>Quizzes</div>
+        <div className="flex flex-wrap gap-10">
+          {quizzes && quizzes.map((quiz, idx) => (
+            <Dialog key={idx}>
+              <DialogTrigger className="px-4 py-3 bg-black text-white rounded-lg">
+                Quiz {idx + 1}
+              </DialogTrigger>
+              <DialogContent className="max-h-[650px] overflow-y-scroll">
+                <DialogHeader>
+                  <DialogTitle>Quiz</DialogTitle>
+                  <DialogDescription>
+                    {quiz && quiz.quiz && quiz.quiz.map((question: any, idx: any) => (
+                      <Card>
+                        <CardHeader>
+                          <CardTitle>
+                            {`Question ${idx + 1}`}
+                          </CardTitle>
+                          <CardDescription>
+                            {question.question}
+                          </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                          <div className="flex flex-col gap-y-5">
+                            <div>Options:</div>
+                            {question.options.map((option: any, idx: any) => (
+                              <div key={idx}>
+                                {option.number}: {option.value}
+                              </div>
+                            ))}
+                            <div>Correct option: {question.correctOption}</div>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    ))}
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
